e1ae8089d2fe033149beb9b83f98bd189e366909
Symoon Zadid <symoon.zadid@brainstation-23.com>
2022-10-12 22:16:00 +0600

    initial comit

---
+++ src/main/java/com/moviedbcrud/MovieDbCrudApplication.java
@@ -0,0 +1,13 @@
+package com.moviedbcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDbCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieDbCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moviedbcrud/MovieDbCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviedbcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDbCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
