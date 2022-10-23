13f1a6b99b07c1b5a4d2c9759b65967fb09e09a9
Symoon Zadid <symoonzadid@gmail.com>
2022-10-23 22:22:31 +0600

    login form validation complete

---
+++ src/main/resources/static/login.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form')
+const adminEmail = document.getElementById('adminEmail')
+const adminPassword = document.getElementById('adminPassword')
+
+function  showError (input,message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+
+form.addEventListener('submit',function (e) {
+    if (adminEmail.value == '') {
+        e.preventDefault();
+        showError(adminEmail, 'Email is Required');
+        return false;
+    }
+    if (adminPassword.value == '') {
+        e.preventDefault();
+        showError(adminPassword, 'Password is Required');
+        return false;
+    }
+})
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,25 +1,55 @@
-var nameError = document.getElementById('name-error')
 
-function validateName(){
-    var name = document.getElementById('movie-name').value;
-    if(name.length == 0){
-        nameError.innerHTML = "Movie Name is Required"
+const form = document.getElementById('form')
+const movieName = document.getElementById('movie-name')
+const movieCategory = document.getElementById('movie-category')
+const movieId = document.getElementById('movie-id')
+const movieRating = document.getElementById('movie-rating')
+const movieLanguage = document.getElementById('movie-language')
+
+function  showError (input, message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+
+form.addEventListener('submit',function (e){
+    if(movieName.value == ''){
+        e.preventDefault();
+        showError(movieName,'Movie Name is Required')
         return false;
     }
-}
-function  checkFormValidation(){
-    if(!validateName()){
-        submitError.innerHTML = "Please fix error to submit";
+    if(movieCategory.value == 'Select Category'){
+        e.preventDefault();
+        showError(movieCategory,'Category  is Required')
+        return false;
     }
-}
+
+    if(movieId.value == ''){
+        e.preventDefault();
+        showError(movieId,'Movie Id is Required')
+        return false;
+    }
+    if(movieRating.value == ''){
+        e.preventDefault();
+        showError(movieRating,'Movie Rating is Required')
+        return false;
+    }
+    if(movieLanguage.value === 'Select Language'){
+        e.preventDefault();
+        showError(movieLanguage,'Movie Language is Required')
+        return false;
+    }
+})
+
 
 function myFunction() {
     alert("Movie Added Successfully!");
 }
 function updateMovie() {
-    alert("Movie updated Successfully!");
+    alert("Movie Updated Successfully!");
 }
 function deleteMovie() {
-    alert("\"Movie delete Successfully\"");
+    alert("Movie Delete Successfully");
 }
 
